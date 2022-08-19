fa07742f6859ed9173cfeae6d5bebd9c39ae19a7
ZakiAnsari <pamidiansari25@gmail.com>
2022-08-20 00:22:10 +0530

    added gloabal exception handler

---
+++ src/main/java/com/masai/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.masai.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.masai.payload.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex)
+	{
+		String msg = ex.getMessage();
+		return new ResponseEntity(new ApiResponse(msg,false),HttpStatus.NOT_FOUND);
+	}
+
+}
