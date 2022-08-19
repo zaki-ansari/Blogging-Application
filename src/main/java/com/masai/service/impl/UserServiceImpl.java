438bd2c7ad64d7a0b25af08e2961016e2dbf5ae4
ZakiAnsari <pamidiansari25@gmail.com>
2022-08-19 21:31:25 +0530

    added hibernate validator dependency

--- src/main/java/com/masai/service/impl/UserServiceImpl.java
+++ src/main/java/com/masai/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.masai.service.impl;
 
 import java.util.List;
-
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +18,9 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserRepo userRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -70,24 +73,29 @@ public class UserServiceImpl implements UserService {
 	
 	private User dtoToUser(UserDto userDto)
 	{
-		User user = new User();
-		user.setId(userDto.getId());
-		user.setName(userDto.getName());
-		user.setEmail(userDto.getEmail());
-		user.setAbout(userDto.getAbout());
-		user.setPassword(userDto.getPassword());
+		
+		User user = modelMapper.map(userDto, User.class);
+		
+//		User user = new User();
+//		user.setId(userDto.getId());
+//		user.setName(userDto.getName());
+//		user.setEmail(userDto.getEmail());
+//		user.setAbout(userDto.getAbout());
+//		user.setPassword(userDto.getPassword());
 		
 		return user;
 	}
 	
 	private UserDto userToDto(User user)
 	{
-		UserDto userDto = new UserDto();
-		userDto.setId(user.getId());
-		userDto.setName(user.getName());
-		userDto.setEmail(user.getEmail());
-		userDto.setAbout(user.getAbout());
-		userDto.setPassword(user.getPassword());
+		
+		UserDto userDto = modelMapper.map(user, UserDto.class);
+//		UserDto userDto = new UserDto();
+//		userDto.setId(user.getId());
+//		userDto.setName(user.getName());
+//		userDto.setEmail(user.getEmail());
+//		userDto.setAbout(user.getAbout());
+//		userDto.setPassword(user.getPassword());
 		
 		return userDto;
 	}
